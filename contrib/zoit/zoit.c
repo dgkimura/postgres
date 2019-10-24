bf158d27898a29f08319de0e0cc64a50111da162
David Kimura <dkimura@pivotal.io>
2019-10-24 00:24:57 +0000

    Add empty index access methods
    Now following works:
    ```
    CREATE EXTENSION zoit;
    CREATE TABLE tst ( i int4, t text);
    CREATE INDEX zoitidx ON tst USING zoit (i, t);
    ```

--- contrib/zoit/zoit.c
+++ contrib/zoit/zoit.c
@@ -1,11 +1,45 @@
 #include "postgres.h"
 
 #include "access/amapi.h"
+#include "nodes/execnodes.h"
 #include "fmgr.h"
 
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(zthandler);
+
+extern bytea *ztoptions(Datum reloptions, bool validate);
+
+bytea *
+ztoptions(Datum reloptions, bool validate)
+{
+	return NULL;
+}
+
+/*
+ * build index from a heap relation that potentially has data.
+ */
+static IndexBuildResult *
+ztbuild(Relation heap, Relation index, IndexInfo *indexInfo)
+{
+	IndexBuildResult *result;
+
+	result = (IndexBuildResult *) palloc(sizeof(IndexBuildResult));
+
+	/* fill this in with actual data... */
+	result->heap_tuples = 0;
+	result->index_tuples = 0;
+
+	return result;
+}
+
+/*
+ * build empty index.
+ */
+static void
+ztbuildempty(Relation index)
+{
+}
 
 Datum
 zthandler(PG_FUNCTION_ARGS)
@@ -29,14 +63,14 @@ zthandler(PG_FUNCTION_ARGS)
 	amroutine->amcaninclude = false;
 	amroutine->amkeytype = InvalidOid;
 
-	amroutine->ambuild = NULL;
-	amroutine->ambuildempty = NULL;
+	amroutine->ambuild = ztbuild;
+	amroutine->ambuildempty = ztbuildempty;
 	amroutine->aminsert = NULL;
 	amroutine->ambulkdelete = NULL;
 	amroutine->amvacuumcleanup = NULL;
 	amroutine->amcanreturn = NULL;
 	amroutine->amcostestimate = NULL;
-	amroutine->amoptions = NULL;
+	amroutine->amoptions = ztoptions;
 	amroutine->amproperty = NULL;
 	amroutine->ambuildphasename = NULL;
 	amroutine->amvalidate = NULL;
