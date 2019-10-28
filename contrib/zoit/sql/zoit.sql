72ac8de20091634976a3db1b30b5d14a0c78fdc8
David Kimura <dkimura@pivotal.io>
2019-10-28 19:21:15 +0000

    Add sql file to test index

---
+++ contrib/zoit/sql/zoit.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION zoit;
+
+CREATE TABLE tst (
+	i	int4,
+	t	text
+);
+
+INSERT INTO tst SELECT i%10, substr(md5(i::text), 1, 1) FROM generate_series(1,2000) i;
+CREATE INDEX zoitidx ON tst USING zoit (i, t);
+
+SET enable_seqscan=off;
+SET enable_bitmapscan=on;
+SET enable_indexscan=on;
+EXPLAIN (COSTS OFF) SELECT * FROM tst WHERE i = 7;
+
+DROP TABLE tst;
+DROP EXTENSION zoit;
