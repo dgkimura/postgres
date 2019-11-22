501b348df6992a99b69b6ac7740dd1a4084d7a8b
David Kimura <dkimura@pivotal.io>
2019-11-22 21:49:27 +0000

    Fix index data to trigger index scan
    Need more than 10 unique values to trigger index scan plan

--- contrib/zoit/sql/zoit.sql
+++ contrib/zoit/sql/zoit.sql
@@ -1,24 +1,16 @@
-DROP TABLE tst;
-DROP EXTENSION zoit;
+DROP TABLE myt;
+DROP EXTENSION zoit CASCADE;
 
 CREATE EXTENSION zoit;
 
-CREATE TABLE tst (
-	i	int4,
-	t	text
-);
+CREATE TABLE myt (i int4);
 
-INSERT INTO tst SELECT i%10, substr(md5(i::text), 1, 1) FROM generate_series(1,2000) i;
-CREATE INDEX zoitidx ON tst USING zoit (i, t);
+CREATE INDEX zoitidx ON myt USING zoit (i int4_ops);
+INSERT INTO myt SELECT i FROM generate_series(1,2000) i;
 CHECKPOINT;
 
-INSERT INTO tst values(42, 'narf');
-
-SET enable_seqscan=off;
-SET enable_bitmapscan=on;
-SET enable_indexscan=on;
 ANALYZE;
-EXPLAIN (COSTS OFF) SELECT * FROM tst WHERE i = 7;
-SELECT * FROM tst WHERE i = 7;
+EXPLAIN (COSTS OFF) SELECT * FROM myt WHERE i = 7;
+SELECT * FROM myt WHERE i = 7;
 
 SELECT relfilenode, relname FROM pg_class WHERE relname LIKE '%zoit%';
